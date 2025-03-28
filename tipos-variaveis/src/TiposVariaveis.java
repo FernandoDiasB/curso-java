781be811303747eef735607cbb91ea99b5a6390d
Fernando Dias <fernando.sdbarros@gmail.com>
2025-03-28 14:28:27 -0300

    commit aula operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // int numero = 5;
+
+        // System.out.println(++numero);
+        // System.out.println(numero);
+
+        int a, b;
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
 
     }
 }
