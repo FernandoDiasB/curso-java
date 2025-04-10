16b91df19797ba646452a5506a8a48bfee89ae51
Fernando Dias <fernando.sdbarros@gmail.com>
2025-04-10 13:01:02 -0300

    aula final

--- herançaPolimorfismo/src/Main.java
+++ herançaPolimorfismo/src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Employee employee = new Employee();
+        Salesman salesman = new Salesman();
         Manager manager = new Manager();
 
-        manager.setName("Maluque");
-        manager.setLogin("maluquinho");
-        manager.setPassword("12746213984");
-
-        System.out.println(manager.getName());
-        System.out.println(manager.getLogin());
-        System.out.println(manager.getPassword());
     }
 }
